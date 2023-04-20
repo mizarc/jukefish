da80667d6db09b0f27d2c2feebdbb4fc243bfa36
Mizarc <mizarc@proton.me>
2023-04-21 02:58:21 +1000

    Add placeholder instance and interfacing with API
    Still trying to figure out this. There is currently a method that allows
    connection to a specific Jellyfin server using hardcoded data, and a
    seperate method that handles trying to display a list of artists.

--- Shared/JamediaApp.swift
+++ Shared/JamediaApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import JellyfinAPI
+import Get
 
 @main
 struct JamediaApp: App {
@@ -13,5 +15,30 @@ struct JamediaApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+func createJellyfinInstance() async -> JellyfinClient? {
+    let test_configuration = JellyfinClient.Configuration(url: URL(string: "testurl")!, client: "JukeFish", deviceName: "TestDev", deviceID: "1", version: "0.1")
+    let jellyfinClient = JellyfinClient(configuration: test_configuration)
+     
+    do {
+        print("got this far 1")
+        let response = try await jellyfinClient.signIn(username: "us", password: "pw")
+        print("got this far 2")
+        return jellyfinClient
+    }
+    catch {
+        return nil
+    }
+}
+
+func displayArtists(jellyfinClient: JellyfinClient) async -> Response<URL>? {
+    do {
+        let query = try await jellyfinClient.download(for: Paths.getArtists())
+        return query
+    }
+    catch {
+        return nil
     }
 }
